6ee2a54fbd4c92940cb29313be88b9e75f545022
Vladimir <serbiant@gmail.com>
2016-12-14 15:48:09 +0300

    Export milestones & labels.

--- bin/processors/label.rb
+++ bin/processors/label.rb
@@ -1,11 +1,30 @@
 class Label < GithubInteractor
 
   def import
-
+    say 'Import called.'
   end
 
   def export
-
+    labels = retrieve_labels
+    save_as_json(labels)
   end
+
+  private
+
+  def retrieve_labels
+    say "Retrieving Github labels from #{@repo}."
+    labels = []
+    page = 0
+
+    loop do
+      single_page_labels = @client.labels(@repo, :page => page)
+      labels += single_page_labels
+      break if single_page_labels.empty?
+      page += 1
+    end
+    say "#{labels.size} labels retrieved."
+    labels.map!(&:to_hash)
+  end
+
 
 end
--- bin/processors/milestone.rb
+++ bin/processors/milestone.rb
@@ -1,11 +1,31 @@
 class Milestone < GithubInteractor
 
-  def import
 
+  def import
+    say 'Import called.'
   end
 
   def export
-
+    milestones = retrieve_milestones
+    save_as_json(milestones)
   end
+
+  private
+
+  def retrieve_milestones(state = :all)
+    say "Retrieving Github milestones from #{@repo}."
+    milestones = []
+    page = 0
+
+    loop do
+      single_page_milestones = @client.list_milestones(@repo, :state => state, :page => page)
+      milestones += single_page_milestones
+      break if single_page_milestones.empty?
+      page += 1
+    end
+    say "#{milestones.size} milestones retrieved."
+    milestones.map!(&:to_hash)
+  end
+
 
 end
